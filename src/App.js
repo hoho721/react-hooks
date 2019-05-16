e03d10b41c3f97c43675d8c9241426b8a781c751
songjongman <sjmsong@gmail.com>
2019-05-17 00:49:49 +0900

    practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import useTabs from "./Components/useTabs";
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the section 2"
+  },
+  {
+    tab: "Section 3",
+    content: "I'm the section 3"
+  },
+  {
+    tab: "Section 4",
+    content: "I'm the section 4"
+  }
+];
 
 function App() {
+
+  const { currentItem, changeItem } = useTabs(0, content);
+
+  const sayHello = () => console.log("hello");
+
+  useEffect(() => {
+    sayHello();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        {
+          content.map((section, idx) =>(
+              <button key={section.tab} onClick={() => changeItem(idx)}>{section.tab}</button>
+          ))
+        }
+        <div>{currentItem.content}</div>
+      </div>
   );
 }
 
